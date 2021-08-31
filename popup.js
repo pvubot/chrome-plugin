39fcd57cc0bdc92e91f0099f2efccb9efb9c088c
2Kem <2Kem@mail.ru>
2021-08-31 20:14:31 +0300

    Marafet

--- popup.js
+++ popup.js
@@ -10,5 +10,10 @@ changePlantVsUndead.addEventListener("click", () => {
 
 let changePanCakeSwap = document.getElementById("PanCakeSwap")
 changePanCakeSwap.addEventListener("click", async () => {
-    window.open('https://pancakeswap.finance/swap', "_blank")
+    window.open('https://pancakeswap.finance/swap?outputCurrency=0x31471e0791fcdbe82fbf4c44943255e923f1b794', "_blank")
+})
+
+let changeTgbot = document.getElementById("TgBot")
+changeTgbot.addEventListener("click", async () => {
+    window.open('https://t.me/pvu_chrome_bot', "_blank")
 })
